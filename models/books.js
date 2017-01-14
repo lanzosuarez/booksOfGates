2c26c355dfe4dfdbe73b886392e9ba9a3e60c9c3
Pons <lanzosuarez@gmail.com>
2017-01-14 22:20:06 +0800

    with models

---
+++ models/books.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+
+    link:{type: String},
+    title: {type: String},
+    author: {type: String},
+    published: {type: String},
+    description: {type: String},
+    imageUrl: {type: String},
+    price: {type: String},
+    createDate: {type: Date},
+    updateDate: {type: Date},
+
+});
+
+module.exports = mongoose.model('Book', schema);
