82f1dd79149d696a65b3412cd9feb9fc933a3ef3
haaalieee <morenoanalie14@gmail.com>
2017-01-29 00:50:04 +0800

    new / edit form changes

--- public/js/script.js
+++ public/js/script.js
@@ -45,6 +45,10 @@ document.ready = (function () {
     // $('#back').click(function(){
     //         window.location = '/';
     // });
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
 
    
 
