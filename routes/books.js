8ab88eb5c957e8840edabdd68f45e59fb33a0b85
Pons <lanzosuarez@gmail.com>
2017-02-05 21:15:33 +0800

    fix

--- routes/books.js
+++ routes/books.js
@@ -30,6 +30,45 @@ router.use((req, res, next)=>{
   next();
 });
 
+//ROUTE FOR NEW BOOK
+router.route('/new')
+    .get((req ,res)=>{
+        //console.log("dsdas")
+        res.render('new');
+    })
+    .post((req, res)=>{
+        console.log("Reached")
+        // \w+\.\w+
+        console.log(req.body)
+        var book = new Book({ //CREATE NEW BOOK INSTANCE
+            link: req.body.link,
+            title: req.body.title,
+            author: req.body.author,
+            published: req.body.published,
+            description: req.body.description,
+            price: req.body.price,
+            imageUrl: req.body.imageUrl,
+            createDate: Date.now(),
+            updateDate: Date.now()
+        }).save((err, doc)=>{ //SAVE BOOK
+            if(err){
+                deletePrev(req.body.imageUrl);
+                console.log(err)//
+                var e = extractErrrors(err);
+                return res.send({
+                    success: false,
+                    respo: e.reverse(),
+                    respoObj: err
+                });
+            }
+            //console.log("Second level")
+            res.status(201).send({
+                success: true,
+                redirect: "/"
+            });
+        })
+    });
+
 router.get("/delete/:id", (req, res, next)=>{
     Book.findById(req.params.id, (err, book)=>{
         if(err) return next();
@@ -132,44 +171,7 @@ router.get('/bookCover/:id',(req, res)=>{
 })
 
 
-//ROUTE FOR NEW BOOK
-router.route('/new')
-    .get((req ,res)=>{
-        console.log("dsdas")
-        res.render('new');
-    })
-    .post((req, res)=>{
-        console.log("Reached")
-        // \w+\.\w+
-        console.log(req.body)
-        var book = new Book({ //CREATE NEW BOOK INSTANCE
-            link: req.body.link,
-            title: req.body.title,
-            author: req.body.author,
-            published: req.body.published,
-            description: req.body.description,
-            price: req.body.price,
-            imageUrl: req.body.imageUrl,
-            createDate: Date.now(),
-            updateDate: Date.now()
-        }).save((err, doc)=>{ //SAVE BOOK
-            if(err){
-                deletePrev(req.body.imageUrl);
-                console.log(err)//
-                var e = extractErrrors(err);
-                return res.send({
-                    success: false,
-                    respo: e.reverse(),
-                    respoObj: err
-                });
-            }
-            //console.log("Second level")
-            res.status(201).send({
-                success: true,
-                redirect: "/"
-            });
-        })
-    });
+
     
 module.exports = router;
 
